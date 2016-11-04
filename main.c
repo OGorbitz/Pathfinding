3efbdf1d6aa264cae0d93614c847897304989b4b
Ozzie Gorbitz <ozziegorbitz@gmail.com>
2016-11-04 11:41:30 -0600

    Blank window created. now to add stuff

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include "gtk+/gtk.h"
+#include "main.h"
 
 static void activate (GtkApplication* app, gpointer user_data)
 {
@@ -7,7 +6,7 @@ static void activate (GtkApplication* app, gpointer user_data)
 
 	window = gtk_application_window_new(app);
 	gtk_window_set_title(GTK_WINDOW(window), "A* Pathfinding Demo");
-	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200)
+	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 	gtk_widget_show_all(window);
 }
 
--- main.h
+++ main.h
@@ -1,3 +1,7 @@
 #include <stdio.h>
-#include <pathfind.h>
+#include "pathfind.h"
+#include <gtk/gtk.h>
 
+static void activate(GtkApplication* app, gpointer user_data);
+
+int main(int argc, char **argv);
